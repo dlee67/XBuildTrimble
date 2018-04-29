12ca135d20c202985ae1f27d6b31f23fe2f0acb4
dlee67 <dlee67@msudenver.edu>
2018-04-28 20:52:55 -0600

    At least, the values are ready to be plotted.

--- Analyzer.py
+++ Analyzer.py
@@ -6,8 +6,11 @@ import cv2 as cv
 import os
 import sys
 from pathlib import Path
+'''
+Since, I will be using feature extraction to figure out the displacements,
+there is no need to use ML techniques.
 
-# Copy pasted from https://youtu.be/z_6fPS5tDNU?list=PLQVvvaa0QuDdttJXlLtAJxJetJcqmqlQq
+Copy pasted from https://youtu.be/z_6fPS5tDNU?list=PLQVvvaa0QuDdttJXlLtAJxJetJcqmqlQq
 def store_raw_images():
     tree_images_links = "http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n13104059"
     tree_images_urls = urllib.request.urlopen(tree_images_links).read().decode()
@@ -30,13 +33,15 @@ def store_raw_images():
                 break;
         except Exception as e:
             print(str(e))
+'''
 
-#Prepares a folder with tree images, and makes the tager images recognizable.
+#Returns the list of file names from the left folder.
 def set_target_path():
     listOfFilesLeft = os.listdir(os.path.join(os.path.dirname(__file__) + "./images/left"))
     listOfFilesLeft.sort()
     listOfFilesRight = os.listdir(os.path.join(os.path.dirname(__file__) + "./images/right"))
     listOfFilesRight.sort()
+    print("List of extracted files from folder is: " + str(listOfFilesLeft))
     return listOfFilesLeft
 
 #https://medium.com/machine-learning-world/feature-extraction-and-similar-image-search-with-opencv-for-newbies-3c59796bf774
@@ -50,12 +55,12 @@ def extract_features(image_path, vector_size=32):
     dsc = dsc.flatten()
     needed_size = (vector_size * 64)
     if dsc.size < needed_size:
-            # if we have less the 32 descriptors then just adding zeros at the
-            # end of our feature vector
             dsc = np.concatenate([dsc, np.zeros(needed_size - dsc.size)])
     return dsc
 
-def convert_to_two_d(flattenedList):
+#From the flattened list, add the values up, so that they are
+#represented as 2 dimensional array.
+def compacted(flattenedList):
     x = 0
     y = 0
     for index in range(0, flattenedList.size):
@@ -65,13 +70,21 @@ def convert_to_two_d(flattenedList):
             y = y + flattenedList[index]
     print("X value now: " + str(x))
     print("Y value now: " + str(y))
+    return [x, y]
 
-sampleNames = set_target_path()
-extracted = extract_features(os.path.join(os.path.dirname(__file__) + "./images/left/" + sampleNames[0]))
-convert_to_two_d(extracted)
+#Prepares the list of 2 dimensional arrays, so that they can be consumed by the matplotlib objects
+#for the plotting.
+def prepare_data_for_plotting():
+    leftFileNames = set_target_path()
+    forPlotting = []
+    for index in range(0, len(leftFileNames)):
+        print("Cosuming file: " + str(os.path.join(os.path.dirname(__file__) + "./images/left/" + leftFileNames[index])))
+        extractedValue = extract_features(os.path.join(os.path.dirname(__file__) + "./images/left/" + leftFileNames[index]))
+        forPlotting.append(compacted(extractedValue)) # Wow, this actually reads like an English.
+    return forPlotting
 
+prepare_data_for_plotting()
 #Verifies that images does show.
-#
 #someImage = cv.imread(os.path.join(os.path.dirname(__file__) + "./images/left/" + listOfFilesLeft[0]), 0)
 #cv.imshow("Die! Kaiser!", someImage)
 #cv.waitKey(0);
